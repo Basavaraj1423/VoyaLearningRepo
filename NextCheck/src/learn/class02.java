32aacb1c2865faffed22d0d0e0704d495872ce0a
Basavaraj R <basavaraj.r2301@gmail.com>
2024-05-28 19:20:28 +0530

    changed class02

--- NextCheck/src/learn/class02.java
+++ NextCheck/src/learn/class02.java
@@ -4,7 +4,7 @@ public class class02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("I am An Perfectionist");
+		System.out.print("I am dumbshit");
 	}
 
 }
