15155ab3cb530ef604a7e34317a4240cbdc29d95
Basavaraj R <basavaraj.r2301@gmail.com>
2024-05-28 19:08:25 +0530

    fourth commit

--- NextCheck/src/learn/class01.java
+++ NextCheck/src/learn/class01.java
@@ -3,8 +3,8 @@ package learn;
 public class class01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("HI");
+		System.out.print("Bye");
 	}
 
 }
---
+++ NextCheck/src/learn/class02.java
@@ -0,0 +1,10 @@
+package learn;
+
+public class class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("I am An Perfectionist");
+	}
+
+}
